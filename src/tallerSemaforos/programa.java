74078c2289223152809ce1b0a266ecf114b97dd3
Javier González González <45976594+javinava2000@users.noreply.github.com>
2020-03-25 15:37:36 +0100

    Add files via upload

---
+++ src/tallerSemaforos/programa.java
@@ -0,0 +1,12 @@
+package tallerSemaforos;
+
+public class programa {
+
+	public static void main(String[] args) {
+		//llamamos a un consumidor y a un productor
+		new producer();
+		new consumer();
+
+	}
+
+}
